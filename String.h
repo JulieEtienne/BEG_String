24b6d199bfbf65b3ad1a840926c8fe06e8a6facc
Pierre Gard <pierre.gard@insa-lyon.fr>
2017-01-16 14:58:44 +0100

    Added attributes to class

--- String.h
+++ String.h
@@ -6,6 +6,15 @@
 
 class String {
 
+  public :
+  
+  protected :
+  
+  
+  private :
+    char* str; // character tab
+    size_t capacity_; // size of tab
+    size_t size; // length of string 
 
 
 };
