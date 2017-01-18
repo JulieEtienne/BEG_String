60aaaa2b202268334761ebcbd162fa59f24a6eec
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-19 00:03:33 +0100

    Testing to figure out bug

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ int main(int argc, char const *argv[])
   //C-string constructor test
   String test3("coucou");
 
+  //String of 251 char
+  String test4("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Bigger >:)");
+  
+  printf("\n Capacity of test 4 :\t %d", test4.capacity());
+  printf("\n Size of test 4 :\t %d", test4.size());
+  printf("\n Content of test 4 :\t %s", test4.c_str());
+
+
+  
   //C-string method test
   printf("\n Test c-string :\t %s", test3.c_str());
 
@@ -30,11 +39,14 @@ int main(int argc, char const *argv[])
   test3.reserve(100);
   printf("\n Capacity of test 3 after reserve :\t %d", test3.capacity());
 
+  //+operator test 
 
 
 
+  //delete test, test2, test3, test4;
 
   printf("\nEND\n");
 
+  return 0;
   return 0;
 }
