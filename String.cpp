35525822c6b662d3ac793c77b09910e37b43bae0
Julie Etienne <julie.etienne11@hotmail.fr>
2017-01-23 18:02:06 +0100

    Added comments to code

--- String.cpp
+++ String.cpp
@@ -1,17 +1,12 @@
 #include "String.h"
 
 
+// CONSTRUCTORS
 
-//DESTRUCTOR
-String::~String()
-{
-  delete [] str;
-}
-
-
-//CONSTRUCTOR
 String::String()
 {
+  //Default constructor
+
   str = new char[255];
 
   str[0]='\0';
@@ -19,9 +14,13 @@ String::String()
   capacity_ = 255;
 }
 
+
 String::String(const String &cp)
 {
+  //Copy constructor
+
   str = new char[cp.capacity_];
+  //Copies the value of cp in str
   for (unsigned int i = 0; i < cp.size_; ++i)
   {
     str[i] = cp.str[i];
@@ -30,102 +29,120 @@ String::String(const String &cp)
   size_ = cp.size_;
   capacity_ = cp.capacity_;
 
+  //Adds the '\0' at the end to close the String
   str[size_]='\0';
 }
 
 
 String::String(const char* c_string)
 {
+  //C-string constructor
+
   int s = 0;
+  //Gets the size of c_string
   while(c_string[s] != '\0')
   {
     ++s;
   }
 
-  size_ = s; // Number of characters (without the '\0')
+  //Number of characters (without the '\0')
+  size_ = s;
 
-  if(size_ < 255)
+  /**Determines if the String fits in the default capacity,
+     Otherwise, sets the capacity to size_+1 or to the maximum
+     required depending on the size_**/
+  if (size_ >= MAX_SIZE)
   {
-    capacity_ = 255;
+    capacity_ = MAX_SIZE;
   }
   else
   {
-    capacity_ = size_ +1;
+    capacity_ = size_ > 255 ? size_+1 : 255;
   }
 
   str = new char[capacity_];
   str[size_] = '\0';
 
-  for(unsigned int i = 0; i < size_; ++i) // Don't copy the '\0' character
+  //Copies the value of c-string and doesn't copy the '\0' character
+  for (unsigned int i = 0; i < size_; ++i)
   {
     str[i] = c_string[i];
   }
 }
 
 
+// DESTRUCTOR
+
+String::~String()
+{
+  //Deletes the char*
+
+  delete [] str;
+}
 
 
-//METHODS
+// MEMBER FUNCTIONS
 
 char* String::c_str()
 {
-  //Returns the c-string.
+  //Returns the c-string
+
   return str;
 }
 
 
 size_t String::size()
 {
-  // Returns the size of the string
+  // Returns the size of the String
+
   return size_;
 }
 
 
 size_t String::max_size()
 {
-  // Returns the maximum size a string can reach due to
-  //the system or library implementations limitations
+  /**Returns the maximum size a string can reach due to
+  the system or library implementations limitations**/
+
   return MAX_SIZE;
 }
 
 
 size_t String::length()
 {
+    //Returns the size of the String
+
     return size_;
 }
 
 
-
-//Returning the size_t capacity
 size_t String::capacity()
 {
   //Returning the capacity
+
   return capacity_;
 }
 
 
-
-//Return True if array empty
 bool String::empty()
 {
   //Return True if array is empty
+
   return size_ == 0 ;
 }
 
 
-
-
-//Allow more size for the string
 void String::reserve(int addedSize)
 {
+  //Allows more size for the String
+
   int a = capacity_ + addedSize;
 
-  //Init of new tab
+  //Initializes of new tab
   char* temp;
-
   temp = new char[a];
 
-  //Fill the new tab of the new string
+  //Fills the new tab with the new String
   for (unsigned int i=0; i <= size_;i++)
     {
       temp[i] = str[i];
@@ -133,21 +150,29 @@ void String::reserve(int addedSize)
   delete [] str;
   str = temp;
 
-  //Update capacity
+  //Updates capacity
   capacity_ += addedSize;
 }
 
 
 void String::resize(size_t n, char c)
 {
-    // resize an initial string to a new size n
+    /**Resizes an initial String to a new size n :
+       If n > size : adds blank boxes or fills them with c
+       If n < size : truncates the String **/
+
+    //Creates a temporary table
     char* tmp = new char[n];
 
+    /**If size if greater than capacity,
+       we need to allow more boxes **/
     if (n >= capacity_)
     {
       this->reserve(int(n-capacity_)+1);
     }
 
+    //Copies the value of the string in tmp
+    //Replaces blank space by char c if needed and asked
     for (unsigned int i = 0; i < n; i++) {
         if (i < size_)
         {
@@ -165,8 +190,10 @@ void String::resize(size_t n, char c)
     str = tmp;
 }
 
+
 void String::clear()
 {
+  //Empties the String
   delete [] str;
   str = new char[255];
   size_ = 0;
@@ -175,8 +202,9 @@ void String::clear()
 }
 
 
-void String::print_str_properties(int id) 
+void String::print_str_properties(int id)
 {
+  //Easier way to view results while testing
   printf("\nString number %d properties\n", id);
   printf("\n Is empty:\t %s", this->empty()? "True":"False");
 
@@ -184,7 +212,6 @@ void String::print_str_properties(int id)
   printf("\n Size:\t\t %d", int(this->size()));
   printf("\n Content:\t %s\n-----------------\n", this->c_str());
 }
-
 
 
 //OPERATORS
@@ -220,7 +247,7 @@ String operator+(const String& lhs, const String& rhs)
 String operator+(const String& lhs, char rhs)
 {
   String sum_s(lhs);
-  
+
   if(sum_s.size_+1 >= sum_s.capacity_)
   {
     sum_s.reserve(int(sum_s.size_-sum_s.capacity_) +1);
@@ -231,15 +258,15 @@ String operator+(const String& lhs, char rhs)
   //Update size
   sum_s.size_ += 1;
   sum_s.str[sum_s.size_] = '\0';
-  
+
   return sum_s;
 }
 
 //Operator+ by char*
-String operator+(const String& lhs, const char* rhs) 
+String operator+(const String& lhs, const char* rhs)
 {
   String sum_s(lhs);
-  
+
   //rhs size
   size_t s = 0;
   while(rhs[s] != '\0') {
--- String.h
+++ String.h
@@ -23,21 +23,20 @@ class String {
     ~String();
 
     // MEMBER FUCTIONS
-    void print_str_properties(int id);
-
 
     size_t capacity();
     size_t size();
     size_t max_size();
     size_t length();
-    void clear();
+    char* c_str();
 
     bool empty();
 
-    char* c_str();
-
+    void clear();
     void resize(size_t n, char c = '\0');
     void reserve(int addedSize);
+
+    void print_str_properties(int id);
 
     //OPERATORS
 
@@ -45,17 +44,18 @@ class String {
     String& operator=(const char* c_string);
     String& operator=(char c);
 
-
-
   private :
 
-    // defines maximum size a string can reach
+    // Defines maximum size a String can reach
     const size_t MAX_SIZE = 4294967291;
 
-    char* str;        // character tab
+    // Character tab
+    char* str;
 
-    size_t capacity_; // size of tab
-    size_t size_;     // length of string
+    // Size of tab
+    size_t capacity_;
+    // Length of String
+    size_t size_;
 
 };
 
