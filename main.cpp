77a18e158d9e1b81aaf746177e46b99a3443027c
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-23 18:04:41 +0100

    Reordered comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "String.h"
-
+#include <iostream>
 
 
 
@@ -7,6 +7,8 @@
 int main()
 {
   printf("\nTP de Cpp\n");
+
+  //----- CONSTRUCTOR TESTS
 
   //Default constructor test
   String test;
@@ -17,70 +19,81 @@ int main()
   //C-string constructor test
   String test3("coucou");
 
-  //String of >255 char
+  //String of >DEF_SIZE char
   String test4("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Bigger >:)");
 
+  //Print str properties test
   test4.print_str_properties(4);
 
+  //----- GETTER TESTS
 
   //C-string method test
   printf("\n Test c-string :\t %s\n", test3.c_str());
 
-  //empty method test
+  //Empty method test
   printf("\n Is empty test3:\t %s\n", test3.empty()? "True":"False");
   printf("\n Is empty test2:\t %s\n", test2.empty()? "True":"False");
 
-  //capacity method test
+  //Capacity method test
   printf("\n Capacity of test 3 :\t %d\n", (int)test3.capacity());
 
-  //size method test
-  printf("\n Size of test 3 :\t %d\n", (int)test3.size());
+  //Size method test (same as length)
+  printf("\n Size of test 3 :\t %d\n", int(test3.size()));
 
-  //reserve method test
+  //Max_size method test
+  std::cout<<"\n"<<test3.max_size()<<std::endl;
+  //printf("\n Max_size : \t%d\n", test3.max_size());
+
+  
+  //----- METHOD TESTS
+
+  //Reserve method test
   test3.reserve(20);
   printf("\n Capacity of test 3 after reserve :\t %d\n", int(test3.capacity()));
 
-  //+operator test
-
-  String test0(test3 + test4);
-
-  test4.print_str_properties(0);
-  
-  //+operator test (char)
-  String testChar = "l'hibou niche ";
-  String resultChar = testChar + 'o' ;
-  
-  resultChar.print_str_properties(42);
-
-  //=operator test for copy, +operator by string
-  test = test3 + test0;
-  test.print_str_properties(1200);
-
-  //=operator test for c_str
-  test = "re-coucou";
-  test.print_str_properties(1);
-
-  //=operator test for char
-  test = 'a';   //Note : also works if character is empty
-  test.print_str_properties(1);
-
-  //resize test
-  test3.resize(300,'u');
-  test3.print_str_properties(3);
-
-  //clear test
+  //Clear test
   test3.clear();
   test3.print_str_properties(3);
 
+  //Resize test
+  test3.resize(99,'u');
+  test3.print_str_properties(3);
+
+
+
+  //----- OPERATOR TESTS
+
+  //Operator+ test String
+  String test0(test3 + test4);
+  test4.print_str_properties(0);
+  
+  //Operator+ test char
+  String testx = "l'hibou niche ";
+  String testy = testx + 'o' ;  
+  testy.print_str_properties(42);
+
+  //Operator+ for char*
   test3.resize(300,'u');
+  char test_c[10] = "Hello";
+  String testz = test3 + test_c;
+
+  testz.print_str_properties(20);
+  testz = testz + " cou cou";
+  testz.print_str_properties(2);
 
 
-  //+operator for char*
-  char test_[10] = "Hello";
-  String t = test3 + test_;
-  t.print_str_properties(20);
-  t = t + " cou cou";
-  t.print_str_properties(2);
+  //Operator= test for copy, +operator by string
+  test = test3 + test0;
+  test.print_str_properties(1200);
+
+  //Operator= test for c_str
+  test = "re-coucou";
+  test.print_str_properties(1);
+
+  //Operator= test for char
+  test = 'a';   //Note : also works if character is empty
+  test.print_str_properties(1);
+
 
   printf("\nEND\n");
 
